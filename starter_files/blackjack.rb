268579dc63f77a08eb2c59f4be73f9f6184099ff
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-16 13:06:24 -0400

    initial comment added, card & deck class

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -1,3 +1,65 @@
-puts "TODO Implement the game of blackjack."
+tputs "TODO Implement the game of blackjack."
 
 # Hint: for starters, read bin/blackjack.rb
+class Card
+  attr_accessor :rank
+  attr_accessor :suit
+  
+  def initialize(rank, suit)
+    @suit = suit
+    @rank = rank
+  end
+
+  def rank_value
+    case @rank
+    when :A
+      1
+    when 1..10
+      @rank
+    when :J
+      10
+    when :Q
+      10
+    when :K
+      10
+    end
+  end
+
+  def ==(other_card)
+    self.suit == other_card.suit &&
+    self.rank == other_card.rank
+  end
+
+  def greater_than?(other_card)
+    self.rank_value > other_card.rank_value
+  end
+end
+
+class Deck
+  attr_accessor :deck
+  def initialize
+      @deck = []
+      create_deck
+  end
+  
+  def create_deck
+      suit_array = [ :clubs, :diamonds, :hearts, :spades ]
+      rank_array = [ :A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+      suit_array.each do |suit|
+        rank_array.each do |rank|
+          @deck.push(Card.new(rank,suit))
+        end
+      end
+  end
+  def draw
+    @deck.shift
+  end
+
+  def shuffle
+    @deck.shuffle!
+  end
+
+  def cards_left
+    @deck.length
+  end
+end
