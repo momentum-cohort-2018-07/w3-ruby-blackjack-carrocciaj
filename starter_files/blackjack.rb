7f9996969332bfabcddf35f1446830935917edef
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-16 15:19:19 -0400

    completed dealing cards to player and adding total

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -41,7 +41,7 @@ class Deck
       @deck = []
       create_deck
   end
-  
+
   def create_deck
       suit_array = [ :clubs, :diamonds, :hearts, :spades ]
       rank_array = [ :A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
@@ -63,3 +63,78 @@ class Deck
     @deck.length
   end
 end
+
+class BlackjackGame
+  attr_accessor :deck
+  attr_accessor :player_hand_array
+  attr_accessor :player_total
+    def initialize
+      @deck = Deck.new
+      @deck.shuffle
+    end
+  
+    def start_game
+      @money = 100
+      puts "Welcome to Josh's Blackjack Table, here is $#{@money} to get you started!"
+    end
+    def round
+      # @bet = 10
+      @player_hand_array = []
+      2.times {player_hand_array << self.player_hand}
+      player_total
+  
+      puts "You have #{self.player_hand_array[0].rank} and #{self.player_hand_array[1].rank}, your total is #{@total}"
+      print 'Do you want to (h)it or (s)tand?'
+      answer = gets.chomp.downcase
+  
+      if answer == 'h' then
+      hit
+      end
+  
+    end
+    def hit 
+        player_hand_array << self.player_hand
+        # @total += self.player_hand_array[2].rank_value
+        player_total
+        puts "You have #{self.player_hand_array[0].rank} #{self.player_hand_array[1].rank} and #{self.player_hand_array[2].rank} your total is #{@total}"
+        print 'Do you want to (h)it or (s)tand?'
+        answer = gets.chomp.downcase
+        if answer == 'h' then
+          hit
+        else
+          @total
+          end
+       
+    end
+  
+    def player_total 
+      @total = 0
+      @player_hand_array.each  do |card|
+        @total += card.rank_value
+      end
+    end
+    def player_hand
+      card = @deck.draw
+      # total = card1.rank_value + card2.rank_value
+      # puts "You have #{card1.rank} and #{card2.rank}, your total is #{total}"
+      
+    end
+  end
+
+# create deck on inititalize
+# start game with greeting
+# deal hand to player
+# return cards and total
+# ask to hit or stand
+# if hit deal one card else submit total
+# if total greater then 21, bust and got to new hand
+# else show final total
+# deal hand to dealer
+# determine if dealer needs to hit or stand
+# return total or bust comment
+# if dealer total > player total
+# return you lose comment
+# else return you win 
+# add or subtract bet to money total
+# deal new hand
+
