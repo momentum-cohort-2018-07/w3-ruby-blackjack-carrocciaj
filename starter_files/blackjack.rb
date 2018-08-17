557a14b03e5447716d16ae7e37a7f9c76eb7556d
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-17 12:02:08 -0400

    added in additional test for card values

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -68,6 +68,11 @@ class BlackjackGame
   attr_accessor :deck
   attr_accessor :player_hand_array
   attr_accessor :player_total
+  attr_accessor :money
+  attr_accessor :dealer_hand
+  attr_accessor :dealer_total
+  attr_accessor :array_to_string
+
     def initialize
       @deck = Deck.new
       @deck.shuffle
@@ -77,51 +82,107 @@ class BlackjackGame
       @money = 100
       puts "Welcome to Josh's Blackjack Table, here is $#{@money} to get you started!"
     end
+
     def round
-      # @bet = 10
-      @player_hand_array = []
-      2.times {player_hand_array << self.player_hand}
+      @bet = 10
+      player_hand
+      # @player_hand_array = []
+      # 2.times {player_hand_array << self.deal_card}
       player_total
-  
+      # player_message
+      puts "You have $#{@money} and you bet $#{@bet}"
       puts "You have #{self.player_hand_array[0].rank} and #{self.player_hand_array[1].rank}, your total is #{@total}"
+      
+      hand_test
+
+      dealer
+    end
+
+    def hit 
       print 'Do you want to (h)it or (s)tand?'
       answer = gets.chomp.downcase
-  
       if answer == 'h' then
-      hit
-      end
-  
-    end
-    def hit 
-        player_hand_array << self.player_hand
+        player_hand_array << self.deal_card
         # @total += self.player_hand_array[2].rank_value
+        
         player_total
-        puts "You have #{self.player_hand_array[0].rank} #{self.player_hand_array[1].rank} and #{self.player_hand_array[2].rank} your total is #{@total}"
-        print 'Do you want to (h)it or (s)tand?'
-        answer = gets.chomp.downcase
-        if answer == 'h' then
-          hit
-        else
-          @total
-          end
-       
+        player_message
+        hand_test
+        # hit
+      elsif answer == 's' then
+        stand
+      end
     end
-  
+
+    def stand
+     puts "Your score is #{@total}!"
+     new_game 
+    end
+    def new_game
+       print 'Do you want to play again (y)es or (n)o?'
+         answer = gets.chomp.downcase
+         if answer == 'y' then
+          self.round
+         else
+          puts 'Thanks for Playing'
+         end
+    end
     def player_total 
       @total = 0
       @player_hand_array.each  do |card|
         @total += card.rank_value
       end
     end
+    def player_array_to_string
+        string = ''
+        @array_to_string = string
+        @player_hand_array.each do |rank|
+          @array_to_string += rank.rank.to_s.center(2.5)
+        end
+    end
+    def player_message
+    player_array_to_string
+    puts "You have #{@array_to_string}your total is #{@total}"
+    end
+    def dealer_total
+      @total_dealer = 0
+      @dealer_hand.each do |card|
+        @total_dealer += card.rank_value
+      end
+    end
+
+    def hand_test
+      if @total == 21 then
+        puts "Blackjack! you win!"
+        new_game
+      elsif @total >= 21 then
+        puts "Bust, you lose!"
+        new_game
+      else
+        hit
+      end
+    end
     def player_hand
+      @player_hand_array = []
+      2.times {player_hand_array << self.deal_card}
+      # player_message
+      # player_total
+      # puts "You have $#{@money} and you bet $#{@bet}"
+      # puts "You have #{self.player_hand_array[0].rank} and #{self.player_hand_array[1].rank}, your total is #{@total}"
+    end
+    def dealer
+      @dealer_hand = []
+      2.times {dealer_hand << self.deal_card}
+      dealer_total
+      puts "Dealer has #{self.dealer_hand[0].rank} and #{self.dealer_hand[1].rank}, your total is #{@total_dealer}"
+
+    end
+    def deal_card
       card = @deck.draw
-      # total = card1.rank_value + card2.rank_value
-      # puts "You have #{card1.rank} and #{card2.rank}, your total is #{total}"
-      
     end
   end
-
 # create deck on inititalize
+# shuffle deck
 # start game with greeting
 # deal hand to player
 # return cards and total
